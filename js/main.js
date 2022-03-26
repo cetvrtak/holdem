aa17838bc33aacfee1c48d39b9209e7238e8e9a8
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-03-26 14:35:24 +0100

    Use deal() generator

---
+++ js/main.js
@@ -0,0 +1,41 @@
+var slots = [p1c1,p1c2,p2c1,p2c2,flip_1,flip_2,flip_3,turn,river];
+
+var it = deal();
+document.getElementById('deal').addEventListener("click", function () {
+	it.next();
+	if (document.getElementById("river").textContent != "")
+	{
+		clearTable();
+		it = deal();
+		it.next();
+	}
+});
+
+function *deal() {
+	for (var slot of slots) {
+		setTimeout(function() {
+			document.getElementById(slot.id).textContent = randomNum() + randomSuit();
+			if (slot.id != "flip_3" && slot.id != "turn")
+			{
+				it.next();
+			}
+		}, 500);
+		yield;
+	};
+};
+
+function clearTable() {
+	for (var slot of slots) {
+		document.getElementById(slot.id).textContent = "";
+	}
+}
+
+function randomNum() {
+	var numbers = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
+	return numbers[Math.floor(Math.random() * numbers.length)];
+};
+
+function randomSuit() {
+	var suits = ["H","S","T","C"];
+	return suits[Math.floor(Math.random() * suits.length)];
+};
